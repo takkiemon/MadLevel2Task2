c416392bbeb3fa0bc76a1583ec7ecab0b76f98fe
taktop <takkie91@hotmail.com>
2020-12-14 08:41:09 +0100

    fully functional. differentiates between left- and right swiping. checks properly if it should be removed or not. Snackbar works as intended

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2task2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val statements = arrayListOf<Statement>()
@@ -29,5 +33,42 @@ class MainActivity : AppCompatActivity() {
             statements.add(Statement(Statement.STATEMENTS[i], Statement.STATEMENT_ANSWERS[i]))
         }
         statementAdapter.notifyDataSetChanged()
+
+        createItemTouchHelper().attachToRecyclerView(rvStatements)
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                if(statements[position].answer && direction == ItemTouchHelper.RIGHT || !statements[position].answer && direction == ItemTouchHelper.LEFT) {
+                    statements.removeAt(position)
+                }
+                else {
+                    onAnswerIncorrect()
+                }
+                statementAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+    private fun onAnswerIncorrect() {
+        Snackbar.make(snackbarLocation, R.string.incorrect, Snackbar.LENGTH_SHORT).show()
     }
 }
